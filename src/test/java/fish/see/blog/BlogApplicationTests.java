1e1e99cc440994e4a43d11d1d4c6fef8e205d51b
Chris Fishback <chrisfishback@fedpowerhouse.attlocal.net>
2024-08-03 21:20:20 -0400

    initial commit

---
+++ src/test/java/fish/see/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package fish.see.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
